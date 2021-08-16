5236aa84a4d810dc8614b8363aec42f62adb7c33
Fr3gU <fr3gu77@gmail.com>
2021-08-16 16:34:23 +0200

    Added test project

---
+++ src/tests/Flowmailer.Test/FlowmailerClientTests/Constructor_Should.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Flowmailer.Test.FlowmailerClientTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void Throw_GivenNullClientId()
+        {
+            Assert.That(() => new FlowmailerClient("asdasdasd", "sdfsdfsdfsdfsfsd", "1234"), Throws.ArgumentNullException);
+        }
+    }
+}
