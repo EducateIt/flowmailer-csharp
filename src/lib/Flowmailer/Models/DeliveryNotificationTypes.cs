53c65b983928ac424a71fd7856dbac0c760d59fd
Thomas Van Machelen <thomas.vanmachelen@hoplr.com>
2023-02-10 15:40:24 +0100

    Add missing properties on SubmitMessage (tags, attachments, etc)

---
+++ src/lib/Flowmailer/Models/DeliveryNotificationTypes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flowmailer.Models
+{
+    public static class DeliveryNotificationTypes
+    {
+        public static string None = "NONE";
+
+        public static string Failure = "FAILURE";
+
+        public static string DeliveryAndFailure = "DELIVERY_AND_FAILURE";
+    }
+}
---
+++ src/lib/Flowmailer/Models/DispositionTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flowmailer.Models
+{
+    public static class DispositionTypes
+    {
+        public static string Attachment = "attachment";
+        public static string Inline = "inline";
+        public static string Related = "related";
+    }
+}
